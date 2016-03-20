9f5f5a3e8effa52bed91dcfc2b2d723e2eba9521
tsgardiner <tsgardiner@windowslive.com>
2016-03-20 19:15:01 +1300

    Completed Lynda Tutorial

--- firstDjango/firstDjango/urls.py
+++ firstDjango/firstDjango/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.core.management.commands.runserver import naiveip_re
+
+from inventory import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^item/(?P<id>\d+)/', views.item_detail, name='item_detail'),
     url(r'^admin/', admin.site.urls),
 ]
--- firstDjango/inventory/views.py
+++ firstDjango/inventory/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from inventory.models import Item
+
+
+def index(request):
+    items = Item.objects.exclude(amount=0)
+    return render(request, 'inventory/index.html', {
+        'items': items,
+
+    })
+
+
+def item_detail(request, id):
+    try:
+        item = Item.objects.get(id=id)
+    except Item.DoesNotExist:
+        raise Http404('This item does not exist')
+    return render(request, 'inventory/item_detail.html', {
+        'item': item,
+    })
